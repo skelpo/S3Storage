7e9919692419d95ad81718387d45857cd1df8c7f
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-12-13 14:58:18 -0600

    Installed LiveUI/S3 package

--- Package.swift
+++ Package.swift
@@ -8,10 +8,11 @@ let package = Package(
         .library(name: "S3Storage", targets: ["S3Storage"]),
     ],
     dependencies: [
+        .package(url: "https://github.com/LiveUI/S3.git", from: "3.0.0-rc2"),
         .package(url: "https://github.com/skelpo/Storage.git", from: "0.1.0")
     ],
     targets: [
-        .target(name: "S3Storage", dependencies: ["Storage"]),
+        .target(name: "S3Storage", dependencies: ["Storage", "S3"]),
         .testTarget(name: "S3StorageTests", dependencies: ["S3Storage"]),
     ]
 )
